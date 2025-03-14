3e75ef12b5a86c2b4a402d22e4d286e1888bab5b
Chris Garcia <chrisgarcia911@outlook.com>
2025-03-14 16:38:39 -0400

    13) added alert dialog for Share Screen

--- ChrisModule8/src/main/java/chris/garcia/n01371506/cg/GarciaActivity8.java
+++ ChrisModule8/src/main/java/chris/garcia/n01371506/cg/GarciaActivity8.java
@@ -2,7 +2,10 @@
 package chris.garcia.n01371506.cg;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -18,9 +21,15 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class GarciaActivity8 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +43,12 @@ public class GarciaActivity8 extends AppCompatActivity implements NavigationView
                 R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.Chrfragment_container, new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
+            navigationView.setCheckedItem(R.id.nav_home);//Default Screen
         }
+
 
         // Handle back button events
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
@@ -52,10 +63,11 @@ public class GarciaActivity8 extends AppCompatActivity implements NavigationView
                 }
             }
         };
-
         // Add the callback to the OnBackPressedDispatcher
         getOnBackPressedDispatcher().addCallback(this, callback);
     }
+
+    //--- Fragment Navigation Code
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -67,7 +79,7 @@ public class GarciaActivity8 extends AppCompatActivity implements NavigationView
             alertPopUp();
         } else if (id == R.id.nav_share) {
             getSupportFragmentManager().beginTransaction().replace(R.id.Chrfragment_container, new ShareFragment()).commit();
-            alertPopUp();
+            alertShareScreen();
         } else if (id == R.id.nav_about) {
             getSupportFragmentManager().beginTransaction().replace(R.id.Chrfragment_container, new AboutFragment()).commit();
             alertPopUp();
@@ -78,6 +90,8 @@ public class GarciaActivity8 extends AppCompatActivity implements NavigationView
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    //--- Alert Dialog ---
     private void alertPopUp() {  // alert popup settings
         new AlertDialog.Builder(this)
                 .setTitle(R.string.chris_garcia)
@@ -93,8 +107,22 @@ public class GarciaActivity8 extends AppCompatActivity implements NavigationView
                 })
                 .setCancelable(false)
                 .show();
-
-
     }
 
+    //--- Share Screen Alert Dialog ---
+    private void alertShareScreen() {
+
+        SimpleDateFormat date = new SimpleDateFormat("hh:mm:ss a", Locale.US);
+        date.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String currentTime = date.format(new Date());
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+
+                .setTitle(R.string.chris_garcia)
+                .setIcon(R.drawable.brazuca)
+                .setMessage(getString(R.string.current_gmt_time)+currentTime)
+                .setPositiveButton(R.string.ok, null)
+                .setCancelable(false)
+                .show();
+    }
 }
